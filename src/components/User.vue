10838a80707c6c7445059b49e18a8074f329c489
larsss1 <lars.otten@student.ehb.be>
2025-08-16 22:32:30 +0200

    added dev4-template-chat-app-vue from dev chat app exercise

---
+++ src/components/User.vue
@@ -0,0 +1,34 @@
+<script>
+    export default {
+        props: {
+            username: String,
+            picture: String
+        }
+    }
+</script>
+
+<template>
+    <div class="user">
+        <div class="profilePic" :style="{ backgroundImage: `url(${picture})` }" >
+        </div>
+        <h4>{{ username }}</h4>
+    </div>
+</template>
+
+<style scoped>
+    .user {
+        display: flex;
+        align-items: center;
+    }
+
+    .user > * {
+        padding: 12px;
+    }
+    .profilePic {
+        width: 40px;
+        height: 40px;
+        border-radius: 20px;
+        background-size: cover;
+        background-position: center;
+    }
+</style>
---
+++ src/views/ChatView.vue
@@ -0,0 +1,44 @@
+<script>
+    import Users from '../components/Users.vue'
+    import Chat from '../components/Chat.vue';
+
+    export default {
+        components: {
+            Chat, Users
+        }
+    }
+</script>
+
+<template>
+  <main>
+    <div class="chatContainer">
+        <Chat/>
+        <form id="chatForm">
+            <input type="text" id="chatInput" placeholder="Send a message" />
+            <input type="hidden" id="chatRoomname" name="chatroomName" value="chatroom1">
+            <input type="hidden" id="userID" name="userID" value="1">
+            <input id="submit" type="submit" value="submit" class="button" />
+        </form>
+    </div>
+    <div class="usersContainer">
+        <Users/>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+    main {
+        display: flex;
+        height: 100%;
+    }
+
+    .chatContainer {
+        width: 66%;
+        height: 80vh;
+    }
+
+    .usersContainer {
+        width: 33%;
+        border-left: 1px solid gray;
+    }
+</style>
